4e1a93742f2c14270eb8418c6a2d5da66d55bb23
hw <hewu10@163.com>
2016-07-01 00:05:33 +0800

    Signed-off-by: hw <hewu10@163.com>

--- src/main/webapp/static/main.js
+++ src/main/webapp/static/main.js
@@ -3,7 +3,7 @@
  */
 requirejs.config({
     //定义requre加载的根目录
-    baseUrl: "/nm/static/lib",
+    baseUrl: "static/lib",
     //css加载（需要依赖require的css.js）
     map: {
         "*": {
@@ -12,62 +12,25 @@ requirejs.config({
     },
     //定义模块间的依赖
     shim: {
-        // foundation: {
-        //     deps: [
-        //         "jquery", "css!../css/foundation/foundation.css", "css!../css/nm-nav.css", "css!../css/foundation/normalize.css","css!../css/common.css"
-        //     ]
-        // },
-        slick: {
+        bootstrap: {
             deps: [
-                "jquery", "css!../lib/slick/slick.css", "css!../lib/slick/slick-theme.css"
+                "jquery", "css!boot/css/bootstrap.css", "css!../css/nm-nav.css", "css!boot/css/bootstrap-theme.css", "css!../css/common.css"
             ]
         }
+
     },
     //模块定义
     paths: {
         "jquery": "jquery-1.9.1",
-        "util": "../module/util",
-        // "foundation": "foundation/foundation.min",
         "domReady": "domReady/domReady",
-        "slick": "slick/slick"
+        "bootstrap": "boot/js/bootstrap"
 
     }
 });
-// //入口加载
-// requirejs(['jquery', "util", "foundation", "slick", "domReady"], function ($, util, f, slick, ready) {
-//     //首页模块加载
-//     //轮播插件
-//     ready(function () {
-//         $('.autoplay').slick({
-//             lazyLoad: 'ondemand',
-//             dots: true,
-//             infinite: true,
-//             speed: 500,
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 2000
-//         });
-//
-//     });
-// });
 //入口加载
-requirejs(['jquery', "util", "slick", "domReady"], function ($, util, slick, ready) {
+requirejs(['jquery', "bootstrap", "domReady"], function ($, boot, ready) {
     //首页模块加载
-    //轮播插件
-    // ready(function () {
-    //     $('.autoplay').slick({
-    //         lazyLoad: 'ondemand',
-    //         dots: true,
-    //         infinite: true,
-    //         speed: 500,
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         autoplay: true,
-    //         autoplaySpeed: 2000
-    //     });
-    //
-    // });
+
     // 初始化首页加载模块
     ready(function () {
         var itemWidth = 0;
